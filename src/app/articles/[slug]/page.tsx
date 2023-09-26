4745b235134cf44dbc904eb8b715c5a782bb8d07
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-09-26 22:09:37 +0200

    set up dynamic routing

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,20 +1,3 @@
-import { getData } from '@/app/api/fetchData';
-
-const data = await getData();
-
-const { posts } = data.props;
-
-export async function generateStaticParams() {
-  return posts.map((post) => ({
-    slug: post.slug,
-  }));
-}
-
-const getPostData = (slug: string) => {
-  return posts.find((post) => post.slug === slug);
-};
-
 export default async function Post({ params }) {
-  const postData = getPostData(params.slug);
-  return <h1>THIS IS WORKING: {postData?.title}</h1>;
+  return <h1>THIS IS WORKING: </h1>;
 }
--- src/app/components/Feed.tsx
+++ src/app/components/Feed.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import Link from 'next/link';
 
 import PostCard from './PostCard';
 
 interface Post {
   id: number;
   title: string;
+  slug: string;
   coverImage: {
     url: string;
   };
@@ -19,14 +20,16 @@ interface FeedProps {
 export const Feed: React.FC<FeedProps> = ({ posts }) => {
   return (
     <div className="flex flex-wrap -mx-2 mt-8">
-      {posts.map(({ id, title, coverImage, excerpt, date }) => (
+      {posts.map(({ id, title, coverImage, excerpt, date, slug }) => (
         <div key={id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-2 mb-4">
-          <PostCard
-            title={title}
-            img={coverImage.url}
-            excerpt={excerpt}
-            date={date}
-          />
+          <Link href={`articles/${slug}`}>
+            <PostCard
+              title={title}
+              img={coverImage.url}
+              excerpt={excerpt}
+              date={date}
+            />
+          </Link>
         </div>
       ))}
     </div>
