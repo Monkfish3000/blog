cbd7357fa2194fa5d03f995c7ed6a85877c49225
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-09-28 18:49:28 +0200

    image and title added to article page

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { getPost } from '@/app/api/fetchData';
 
 interface PostParams {
@@ -12,8 +14,17 @@ export default async function Post({ params }: PostParams) {
   const { id, date, title, content, coverImage } = post[0];
 
   return (
-    <article>
-      <h1>{title}</h1>
-    </article>
+    <header className="relative w-full h-[80vh]">
+      <Image
+        className="object-cover w-full h-full scale-[1.07]"
+        src={coverImage.url}
+        alt="Hero Image"
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className="absolute bottom-0 left-0 right-0 p-5 bg-white text-center mx-auto w-[75%]">
+        <h1 className="text-3xl font-extrabold">{title}</h1>
+      </div>
+    </header>
   );
 }
