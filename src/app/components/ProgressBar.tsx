e3cdff45a7509c4c01f419adeab829bf2f09a885
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-10-16 21:10:06 +0200

    scroll progress function added to Progress Bar

--- src/app/components/ProgressBar.tsx
+++ src/app/components/ProgressBar.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+'use client';
+
+import { useState, useEffect, useRef } from 'react';
 
 const ProgressBar = () => {
-  return <div>ProgressBar</div>;
+  const [scrollProgress, setScrollProgress] = useState(0);
+  const progressBarRef = useRef(null);
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.body.clientHeight;
+      const scrollTop = window.scrollY;
+      const fullScrollHeight = documentHeight - windowHeight;
+
+      const progress = (scrollTop / fullScrollHeight) * 100;
+      setScrollProgress(Math.floor(progress));
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(scrollProgress);
+  }, [scrollProgress]);
+
+  return (
+    <span
+      ref={progressBarRef}
+      style={{ transform: `translateX(${50 - 100}%)` }}
+      className="bg-customBlue h-1 w-full absolute bottom-0"
+    />
+  );
 };
 
 export default ProgressBar;
