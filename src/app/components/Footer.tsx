55c8f812f8aa6c9ce737e7cb2805b818d218c3b8
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-11-10 10:39:31 +0100

    footer text in center

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-customBlack text-white dark:text-customBlack dark:bg-white">
-      <p className="p-4">&copy; 2023 Monkfish. All rights reserved.</p>
+    <footer className="w-full bg-customBlack text-white dark:text-customBlack dark:bg-white content-center flex items-center justify-center">
+      <p className="p-4 m">&copy; 2023 Monkfish. All rights reserved.</p>
     </footer>
   );
 };
