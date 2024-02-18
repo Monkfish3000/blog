006d38eff7679c88cb4e2ccaf159d51f0895728b
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2024-02-18 22:04:11 +0100

    about profile written

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,45 +1,38 @@
 import { Header } from '../components';
+import Footer from '../components/Footer';
 
 const About = () => {
   return (
     <>
       <Header title={'About Me'} />
       <section className="m-auto mt-16 w-2/5">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum neque
-        sequi minus, quam corrupti rerum nulla quas asperiores culpa, illo,
-        quisquam minima laboriosam nostrum dolor nam inventore repellendus
-        delectus nisi. Aspernatur dolor doloribus, adipisci quas perferendis
-        odit iure nam natus mollitia repellendus quae molestias unde pariatur
-        provident quam. Quaerat sapiente eos eveniet aliquam nihil sequi iusto
-        dolorum libero blanditiis suscipit! Quos esse dolorum soluta odit hic
-        ipsum reprehenderit sequi, molestiae officia maxime ullam eos veritatis
-        et ducimus cum cumque, harum tempora. Inventore aliquid porro dolores
-        omnis iure enim corporis earum. Aspernatur, repellendus deserunt iste,
-        id sequi cupiditate incidunt mollitia quos quidem accusamus eius alias,
-        excepturi adipisci facilis repellat. Ipsam atque nulla dolores
-        asperiores reprehenderit vitae repudiandae unde iste amet adipisci!
-        Similique, ad totam. Autem cupiditate repellendus cum perspiciatis ipsa
-        maiores dolorum enim vel ducimus. Nobis sapiente possimus accusantium
-        mollitia corrupti consequatur eius maiores exercitationem quas eos
-        similique, repellendus id excepturi! Aperiam error dolorum perspiciatis
-        nam deserunt sit quibusdam nulla harum necessitatibus aut sunt officia
-        aliquid autem nesciunt optio totam quia rerum, ipsa impedit maiores
-        suscipit unde deleniti! Quasi, animi dolores? Facere suscipit unde a
-        ipsum voluptatem aperiam ratione culpa minima dolorem quam iste,
-        molestias totam esse. Fugiat nesciunt odit autem non dignissimos
-        voluptatum aspernatur impedit ea, blanditiis saepe, hic ratione. Eum
-        eligendi totam quo quisquam ad officia incidunt non quod nesciunt
-        mollitia, magni ipsa perferendis assumenda repudiandae nisi minima
-        quaerat impedit harum exercitationem unde. Deserunt voluptatem error
-        dolores quos harum? Veritatis temporibus, laudantium recusandae eveniet
-        veniam maiores! Cumque, amet quidem. Nostrum odit modi tempora nobis
-        eaque autem deserunt, nemo laboriosam ratione delectus quas, similique
-        corporis, non porro suscipit atque saepe! Placeat quisquam odit
-        assumenda accusamus quia earum voluptatum, debitis dignissimos et?
-        Expedita sed, libero ducimus quas consequuntur facere adipisci error
-        sint repellat repudiandae neque, dolores, atque alias iure nemo
-        quibusdam!
+        <h1 className="font-bold text-3xl mb-6">About MonkFish</h1>
+        <p className="mb-40">
+          Welcome to my digital fishpond! I'm Michael, also known as MonkFish.
+          Born with a knack for code and a heart for innovation, I've carved my
+          path as a Software Engineer, where the vibrant city of Barcelona
+          serves as the backdrop to my tech adventures. In the ever-evolving
+          landscape of EdTech, I stand at the crossroads where education meets
+          technology. It's more than just writing code; it's about building
+          bridges to knowledge. My journey in tech is fueled by an insatiable
+          curiosity and a relentless pursuit of growth. From the intricate dance
+          of CSS and JavaScript to the strategic structuring with React, each
+          line of code is a step towards mastering the art of development. But
+          it's not just about the tech. It's about the people, the learning, and
+          the impact we can make together. Outside the matrix of development
+          environments, you'll find me exploring the latest tech trends, diving
+          deep into the world of open source, or sharing insights with fellow
+          enthusiasts. This blog is my sandbox—a place to experiment, share, and
+          connect with you, whether you're a seasoned developer, an EdTech
+          aficionado, or simply intrigued by the power of technology to
+          transform education. So, whether you're here for a deep dive into the
+          frontend realm, a glimpse into the EdTech revolution, or just a good
+          old tech talk, you're in the right place. Let's embark on this journey
+          together, navigating the exciting world of technology, one post at a
+          time.
+        </p>
       </section>
+      <Footer />
     </>
   );
 };
