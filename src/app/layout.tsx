f737d74fc5e4a269e4a465cfa16d0d836837701a
Michael <michael@Michaels-Laptop.local>
2024-08-02 13:23:33 +0200

    font optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
-import './styles/globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import Providers from './providers';
+import "./styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Providers from "./providers";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
-  title: 'The Fish Pond',
-  description: 'Monkfish blog',
+  title: "The Fish Pond",
+  description: "Monkfish blog",
 };
 
 export default function RootLayout({
@@ -17,6 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="preload"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+          as="style"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+        />
+      </head>
       <body className={inter.className}>
         <Providers>
           <main>{children}</main>
